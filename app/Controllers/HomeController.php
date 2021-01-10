2de5bb5e51a6b69ee46db48d4132349091516c67
alfast456 <56823267+alfast456@users.noreply.github.com>
2021-01-10 23:58:00 +0700

    Summary

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+class HomeController extends Controller{
+
+    public function test($name){
+        echo $name;
+    }
+
+    public function index(){
+        echo 'ini default metode';
+    }
+}
